2e7aab1623e6b3085689609f104cfab05cda73d7
Milton Palmeira <lpsantana_@hotmail.com>
2025-09-08 22:30:20 -0300

    add: pegar todos os elementos do form e inserir em um json com foreach

--- js/index.js
+++ js/index.js
@@ -1,9 +1,34 @@
-document.querySelector('#exampleInputName').value;
-document.querySelector('#exampleInputGenderM').value;
-document.querySelector('#exampleInputGenderF').value;
-document.querySelector('#exampleInputBirth').value;
-document.querySelector('#exampleInputCountry').value;
-document.querySelector('#exampleInputEmail1').value;
-document.querySelector('#exampleInputPassword1').value;
-document.querySelector('#exampleInputFile').value;
-document.querySelector('#exampleInputAdmin').value;
+let fields = document.querySelectorAll('#form-user-create [name]');
+
+let json = {};
+
+fields.forEach(function(field, index){
+    if (field.name == 'gender') {
+        if (field.checked) {
+            json[field.name] = field.value;
+        }
+    }
+    else if (field.name == 'admin') {
+        if (field.checked) {
+            json[field.name] = "Sim";
+        }
+        else {
+            json[field.name] = "Não";
+        }
+    }
+    else {
+        json[field.name] = field.value;
+    }
+});
+
+console.log(json);
+
+// let name = document.querySelector('#exampleInputName').value;
+// let gender = document.querySelector('input[type=radio]:checked').value;
+// let birth = document.querySelector('#exampleInputBirth').value;
+// let country = document.querySelector('#exampleInputCountry').value;
+// let email = document.querySelector('#exampleInputEmail1').value;
+// let password = document.querySelector('#exampleInputPassword1').value;
+// //let photo = document.querySelector('#exampleInputFile').value;
+// let admin = document.querySelector('#exampleInputAdmin').checked;
+// let button = document.querySelector('#save');
